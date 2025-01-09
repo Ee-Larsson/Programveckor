87f748c8c00184303f762f7e138a7a56e1eb2c12
William2057 <Williamarnell882@gmail.com>
2025-01-09 22:11:34 +0100

    .

--- Assets/UglyAxeScript.cs
+++ Assets/UglyAxeScript.cs
@@ -4,33 +4,18 @@ using UnityEngine;
 
 public class NPCInteraction : MonoBehaviour
 {
-    bool isplayernearby = false;
-    public GameObject diolog;
+    public string Axe;
 
     // Start is called before the first frame update
-    void Start()
+    public void interact()
     {
-
+        Debug.Log("Interacted with " + Axe);
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && isplayernearby == true)
-        {
-            print("Spelaren vill plocka upp yxa");
-            diolog.SetActive(true);
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        print("Något är i närheten");
-        isplayernearby = true;
-
-    }
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        isplayernearby = false;
-        diolog.SetActive(false);
+        
     }
 }
