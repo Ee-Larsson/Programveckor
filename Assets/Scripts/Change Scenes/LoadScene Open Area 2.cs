bba64f22e4e7f15bf379f6577706bee6934f9cf6
William2057 <Williamarnell882@gmail.com>
2025-01-13 14:03:30 +0100

    scenes

---
+++ Assets/Scripts/Change Scenes/LoadScene Open Area 2.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOpenArea : MonoBehaviour
+{
+    bool isplayernearby = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isplayernearby == true)
+        {
+            print("Spelaren vill prata med NPC");
+            SceneManager.LoadScene(4);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Något är i närheten");
+        isplayernearby = true;
+
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        isplayernearby = false;
+        
+    }
+}
+
