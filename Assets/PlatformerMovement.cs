d4c1945648a755ff2ce80409d49af38433b52192
Lehto1 <harry.lehto13@gmail.com>
2025-01-14 10:37:12 +0100

    Dog

---
+++ Assets/PlatformerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerMovement : MonoBehaviour
+{
+    public Rigidbody2D RB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RB.velocity = new Vector2(0, 0);
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            RB.velocity = new Vector2(10, 0);
+        } else if (Input.GetKeyDown(KeyCode.A))
+        {
+            RB.velocity = new Vector2(-10, 0);
+        }
+    }
+}
