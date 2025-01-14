fda29c61a3d1a5fc7b0116efc6b9f10568a7bf1d
Ee-Larsson <eemil.larsson@elev.ga.lbs.se>
2025-01-14 09:46:28 +0100

    renamed objects

--- Assets/Scripts/Change Scenes/LoadScene Village.cs
+++ Assets/Scripts/Change Scenes/LoadScene Village.cs
@@ -18,6 +18,7 @@ public class LoadSceneVillage : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && isplayernearby == true)
         {
             print("Spelaren vill prata med NPC");
+
             SceneManager.LoadScene(2);
         }
     }
