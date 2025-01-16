ee8754239d8ebed98ed59b6e80a8ff28811b3196
William2057 <Williamarnell882@gmail.com>
2025-01-16 20:47:25 +0100

    Skapat Cave Entrance

---
+++ Assets/Scripts/Change Scenes/LoadScene Cave Entrance.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneCaveEntrance : MonoBehaviour
+{
+    public bool isplayernearby = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isplayernearby == true)
+        {
+            print("Spelaren vill prata med NPC");
+            SceneManager.LoadScene(8);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Något är i närheten");
+        isplayernearby = true;
+
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        isplayernearby = false;
+
+    }
+}
