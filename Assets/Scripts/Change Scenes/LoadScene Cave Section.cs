18cd763df6ff534de76169f872ffd131dee6aead
William2057 <Williamarnell882@gmail.com>
2025-01-17 11:20:32 +0100

    Did Something

--- Assets/Scripts/Change Scenes/LoadScene Cave Section.cs
+++ Assets/Scripts/Change Scenes/LoadScene Cave Section.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadScene : MonoBehaviour
+public class LoadSceneCaveSection : MonoBehaviour
 {
     public bool isplayernearby = false;
     // Start is called before the first frame update
@@ -11,14 +11,14 @@ public class LoadScene : MonoBehaviour
     {
 
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (isplayernearby == true && Input.GetKeyDown(KeyCode.E))
+
+        if (isplayernearby == true)
         {
             print("Spelaren vill prata med NPC");
-            SceneManager.LoadScene(13);
+            SceneManager.LoadScene(12);
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
